ca6b0feac5e914cd79ee7deef568c18380c50773
Mayro Myller <mayro@fidelizi.com.br>
2026-05-05 17:53:01 -0300

    fix: capitalize was not handling multiple words correctly

--- capitalize.ts
+++ capitalize.ts
@@ -2,4 +2,5 @@ function capitalize(str: string): string {
   return str.replace(/\b\w/g, (c) => c.toUpperCase());
 }
 
-console.log(capitalize('hello world')); // Hello World
+console.log(capitalize('hello world'));   // Hello World
+console.log(capitalize('the quick fox')); // The Quick Fox
